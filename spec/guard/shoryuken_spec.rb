83decbe20bd6b4d957cbb640cc4f5eebf6916a29
Eduardo Sebastian <esebastian@altiora.es>
2015-04-26 13:04:42 +0200

    accept actual shoryuken options

--- spec/guard/shoryuken_spec.rb
+++ spec/guard/shoryuken_spec.rb
@@ -4,17 +4,11 @@ require 'guard/compat/test/helper'
 describe Guard::Shoryuken, exclude_stubs: [Guard::Plugin] do
   describe 'start' do
 
-    it 'accepts :environment option' do
-      environment = :foo
+    it 'should accept :concurrency option' do
+      concurrency = 10
 
-      obj = Guard::Shoryuken.new :environment => environment
-      obj.send(:cmd).should include "--environment #{environment}"
-    end
-
-    it 'accepts :logfile option' do
-      logfile = 'log/shoryuken.log'
-      obj = Guard::Shoryuken.new :logfile => logfile
-      obj.send(:cmd).should include "--logfile #{logfile}"
+      obj = Guard::Shoryuken.new :concurrency => concurrency
+      obj.send(:cmd).should include "--concurrency #{concurrency}"
     end
 
     describe 'with :queue option' do
@@ -33,42 +27,50 @@ describe Guard::Shoryuken, exclude_stubs: [Guard::Plugin] do
       end
     end
 
-    it 'should accept :timeout option' do
-      timeout = 2
-
-      obj = Guard::Shoryuken.new :timeout => timeout
-      obj.send(:cmd).should include "--timeout #{timeout}"
-    end
-
-    it 'should accept :concurrency option' do
-      concurrency = 2
-
-      obj = Guard::Shoryuken.new :concurrency => concurrency
-      obj.send(:cmd).should include "--concurrency #{concurrency}"
+    it 'should accept :require option' do
+      obj = Guard::Shoryuken.new :require => './shoryuken_helper.rb'
+      obj.send(:cmd).should include '--require ./shoryuken_helper.rb'
     end
 
     it 'should accept :config option' do
-      config = 'shoryuken.yml'
+      config = 'config/my_config.yml'
 
       obj = Guard::Shoryuken.new :config => config
       obj.send(:cmd).should include "-C #{config}"
     end
 
+    it 'should accept :rails option' do
+      obj = Guard::Shoryuken.new :rails => true
+      obj.send(:cmd).should include '--rails'
+    end
+
+    it 'accepts :logfile option' do
+      logfile = 'log/my_logfile.log'
+      obj = Guard::Shoryuken.new :logfile => logfile
+      obj.send(:cmd).should include "--logfile #{logfile}"
+    end
+
+    it 'accepts :pidfile option' do
+      logfile = 'my_pidfile.pid'
+      obj = Guard::Shoryuken.new :logfile => logfile
+      obj.send(:cmd).should include "--logfile #{logfile}"
+    end
 
     it 'should accept :verbose option' do
       obj = Guard::Shoryuken.new :verbose => true
       obj.send(:cmd).should include '--verbose'
     end
 
-    it 'should accept :require option' do
-      obj = Guard::Shoryuken.new :require => './shoryuken_helper.rb'
-      obj.send(:cmd).should include '--require ./shoryuken_helper.rb'
-    end
-
     it 'should provide default options' do
       obj = Guard::Shoryuken.new
       obj.send(:cmd).should include "--concurrency #{Guard::Shoryuken::DEFAULT_CONCURRENCY}"
       obj.send(:cmd).should include '--verbose'
+      obj.send(:cmd).should_not include '--queue'
+      obj.send(:cmd).should_not include '--require'
+      obj.send(:cmd).should_not include '-C'
+      obj.send(:cmd).should_not include '--rails'
+      obj.send(:cmd).should_not include '--logfile'
+      obj.send(:cmd).should_not include '--pidfile'
     end
 
   end
