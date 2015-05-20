5fa66f70ef657e7cf3f3c39d6ea2267b36f72cc3
Eduardo Sebastian <esebastian@altiora.es>
2015-05-20 16:55:03 +0200

    bump version to 0.1.1

--- lib/guard/shoryuken/version.rb
+++ lib/guard/shoryuken/version.rb
@@ -2,7 +2,7 @@ module Guard
   module ShoryukenVersion
     MAJOR = 0     # api
     MINOR = 1     # features
-    PATCH = 0    # bug fixes
+    PATCH = 1    # bug fixes
     BUILD = nil   # beta, rc1, etc
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
